f4424f09d766410e2131bbae0e454687888fcbf1
yeongeun-Kim <1787forwork@gmail.com>
2023-01-10 10:01:58 +0900

    update java_p

---
+++ Java_w3r/DataTypes/src/com/w3resource/datatypes/N6.java
@@ -0,0 +1,13 @@
+package com.w3resource.datatypes;
+
+import java.util.Scanner;
+
+public class N6 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		scan.close();
+	}
+}
